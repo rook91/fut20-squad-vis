0f917d20e188f03d307dfb4a7b9fa8be01150a6b
rook91 <rook01093@gmail.com>
2020-08-29 22:45:48 +0200

    parse players names

--- src/Table.tsx
+++ src/Table.tsx
@@ -3,11 +3,30 @@ import { AgGridReact } from "ag-grid-react";
 import "ag-grid-enterprise";
 import { GridOptions } from "../node_modules/ag-grid-community/dist/lib/entities/gridOptions"
 import { ColDef } from "../node_modules/ag-grid-community/dist/lib/entities/colDef"
+
 import players from './players.json'
+import playersMeta from './players-meta.json';
+
+const getPlayersNames = (players) => {
+    const pNames = {};
+    players.LegendsPlayers.forEach(item => {
+        pNames[item.id] = item.c !== undefined ? item.c : `${item.f} ${item.l}`;
+    });
+    players.Players.forEach(item => {
+        pNames[item.id] = item.c !== undefined ? item.c : `${item.f} ${item.l}`;
+    });
+    return pNames;
+};
+
+const parsePlayersNames = (players, playersMeta) => players.itemData.map(i => {
+    const newItem = i;
+    newItem.assetId = playersMeta[i.assetId]
+    return newItem;
+});
 
 const columnDefs: ColDef[] = [
     {
-        headerName: "Asset ID",
+        headerName: "Name",
         field: "assetId",
     },
     {
@@ -57,10 +76,13 @@ const gridOptions: GridOptions = {
 }
 
 export default function Table() {
+    const pMeta = getPlayersNames(playersMeta);
+    const parsdPlayers = parsePlayersNames(players, pMeta);
+
     return (
         <AgGridReact
             gridOptions={gridOptions}
-            rowData={players.itemData}
+            rowData={parsdPlayers}
         />
     );
 }
