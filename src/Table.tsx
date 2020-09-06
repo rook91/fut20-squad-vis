8ee39233109b8ea2242300d53196772a23eedc88
rook91 <rook01093@gmail.com>
2020-09-06 20:41:53 +0200

    table component and locale file

--- src/Table.tsx
+++ src/Table.tsx
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
 import { AgGridReact } from "ag-grid-react";
 import "ag-grid-enterprise";
+import dot from 'dot-object';
+
 import { GridOptions } from "../node_modules/ag-grid-community/dist/lib/entities/gridOptions"
 import { ColDef } from "../node_modules/ag-grid-community/dist/lib/entities/colDef"
 
+
 import players from './players.json'
 import playersMeta from './players-meta.json';
+import locale from './locale_PL.json';
 
 const getPlayersNames = (players) => {
     const pNames = {};
@@ -23,6 +27,41 @@ const parsePlayersNames = (players, playersMeta) => players.itemData.map(i => {
     newItem.assetId = playersMeta[i.assetId]
     return newItem;
 });
+
+const redefineKeys = () =>{
+
+}
+
+const parseLocale = (locale) => {
+    let parsedLocale = {};
+    let counter = 0;
+    Object.keys(locale).forEach((element, index, array) => {
+        // if (index > 1500 && index < 2000) {
+            let parsedKey = atob(element);
+            const nextIndex = index + 1;
+            if (nextIndex < array.length) {
+                let parsedNextKey = atob(array[nextIndex]);
+                const subCheck = `${parsedKey}.`
+                // console.log(`check: ${parsedNextKey} / ${subCheck}`);
+                if (parsedNextKey.indexOf(subCheck) !== -1) {
+                    // console.log(`changed: ${parsedNextKey} / ${subCheck}`);
+                    parsedKey += '.defaultLabel';
+                }
+                // if (parsedLocale.parsedKey) {
+                //     console.log(`has property: ${parsedKey}`);
+                //     parsedKey += '.next';
+                // }
+            }
+            // const replaceDown = parsedKey.replaceAll("_", ".");
+            parsedLocale[parsedKey] = atob(locale[element]);
+        // }
+    });
+    // console.log(parsedLocale);
+    dot.object(parsedLocale);
+    console.log(parsedLocale);
+    return parsedLocale;
+};
+
 
 const columnDefs: ColDef[] = [
     {
@@ -78,7 +117,7 @@ const gridOptions: GridOptions = {
 export default function Table() {
     const pMeta = getPlayersNames(playersMeta);
     const parsdPlayers = parsePlayersNames(players, pMeta);
-
+    parseLocale(locale);
     return (
         <AgGridReact
             gridOptions={gridOptions}
